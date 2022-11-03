5ed5150c178263922f297e1e663c7ba6222218c7
mahimagupta24 <mahimagupta2401@gmail.com>
2022-11-03 20:38:45 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+
+const items=[
+    {
+        title:'What is React?',
+        content:'React is front end javascript framework.'
+    },
+    {
+      title:'Why use React?',
+      content:'React is a favourite javascript libraray among engineers'
+    },
+    {
+        title:'How do you use React?',
+        content:'We use React by creating components'
+    }
+    
+]
+
+
 
 const App=()=>{
-    return <h1>widget app</h1>
-}
+return <div><Search/></div>
+
+    // return <div><Accordion items={items}/></div>
+}//after accordian items is prop which is passed in accordion and in curly braces is array mentioned above
 export default App;
