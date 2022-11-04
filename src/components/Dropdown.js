cd446b884290c7cff77ee4c7238833574f42afdf
mahimagupta24 <mahimagupta2401@gmail.com>
2022-11-04 21:26:31 +0530

    add new component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,52 @@
+import React, { useState,useEffect} from "react";
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    document.body.addEventListener("click", () => {
+      console.log("body click");
+      setOpen(false);
+    },
+    {capture:true});
+  }, []);
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+          console.log("item click");
+          onSelectedChange(option);
+        }}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="select">Select a color</label>
+        <div
+          onClick={() => {
+            console.log("dropdown click");
+            setOpen(!open);
+          }}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
