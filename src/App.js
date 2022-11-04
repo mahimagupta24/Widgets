59e969c5b99cb7b7fc701b194bfd5c01ca53f93b
mahimagupta24 <mahimagupta2401@gmail.com>
2022-11-04 21:27:08 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-const items=[
-    {
-        title:'What is React?',
-        content:'React is front end javascript framework.'
-    },
-    {
-      title:'Why use React?',
-      content:'React is a favourite javascript libraray among engineers'
-    },
-    {
-        title:'How do you use React?',
-        content:'We use React by creating components'
-    }
-    
-]
+const items = [
+  {
+    title: "What is React?",
+    content: "React is front end javascript framework.",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favourite javascript libraray among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "We use React by creating components",
+  },
+];
+const options = [
+  {
+    label: "The color Red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "The shadow Blue",
+    value: "blue",
+  },
+];
 
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
+    </div>
+  );
 
-
-const App=()=>{
-return <div><Search/></div>
-
-    // return <div><Accordion items={items}/></div>
-}//after accordian items is prop which is passed in accordion and in curly braces is array mentioned above
+  // return <div><Accordion items={items}/></div>
+}; //after accordian items is prop which is passed in accordion and in curly braces is array mentioned above
 export default App;
